37e476a8993eb42807eb992265027e5cbcfeefa0
Paul <pvigar@gmail.com>
2019-07-21 16:17:56 -0400

    added scss for infoBar

--- src/components/infoBar/infoBar.jsx
+++ src/components/infoBar/infoBar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import styles from './infoBar.module.scss'
 
 export default class InfoBar extends React.Component {
   render() {
     return (
-    <div id="infobar">
+    <div className={styles.infoBar}>
         <h1>Test Banner</h1>
+
     </div>
 
 
