53d5281ae1c6b725f34faf767cf6b5bab20a878d
Paul <pvigar@gmail.com>
2019-07-21 15:54:50 -0400

    Added InfoBar, update File structure

---
+++ src/components/infoBar/infoBar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class InfoBar extends React.Component {
+  render() {
+    return (
+    <div id="infobar">
+        <h1>Test Banner</h1>
+    </div>
+
+
+    )
+  }
+}
