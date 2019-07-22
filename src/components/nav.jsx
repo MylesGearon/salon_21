67df7ac338c00064a0846521ba59bccf75cc97cf
Unknown <myles@mylesgearon.com>
2019-07-22 11:12:38 -0400

    Fix small logo

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 
 import styles from './nav.module.scss';
+import smallLogo from '../assets/small_logo.svg';
 
 const Nav = props => {
 
   return (
     <div className={styles.container}>
-      <div>logo</div>
-      <div>Link</div>
-      <div>Link</div>
-      <div>Link</div>
+      <img className={styles.logoContainer} src={smallLogo} />
       <div className={styles.emailContainer}>
         <input placeholder="Email" />
         <button >Signup</button>
