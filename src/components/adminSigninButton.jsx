3a9a0cc7b825e30275b362164f511a38adbfda75
Unknown <myles@mylesgearon.com>
2019-07-22 10:08:32 -0400

    Extract admin sign button component

---
+++ src/components/adminSigninButton.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import netlifyIdentity from 'netlify-identity-widget';
+
+const handleAdminClick = () => {
+  if (netlifyIdentity.currentUser()) {
+    window.location.href = `${window.location.origin}/admin`;
+  } else {
+    netlifyIdentity.open();
+  }
+}
+
+export default () => {
+  useEffect(() => {
+    netlifyIdentity.init({container: '#identity-service'});
+  }, [null])
+
+  return (
+    <button
+      id="identity-service"
+      onClick={handleAdminClick}
+    >
+      Login with Netlify Identity
+    </button>
+  )
+};
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,31 +1,15 @@
-import React, { useEffect } from 'react';
-import netlifyIdentity from 'netlify-identity-widget';
-import { graphql, StaticQuery } from 'gatsby';
-import Img from "gatsby-image"
+import React from 'react';
 
 import styles from './nav.module.scss';
 
-const handleAdminClick = () => {
-  console.log(netlifyIdentity.currentUser());
-  if (netlifyIdentity.currentUser()) {
-    window.location.href = `${window.location.origin}/admin`;
-  } else {
-    netlifyIdentity.open();
-  }
-}
-
 const Nav = props => {
-  useEffect(() => {
-    netlifyIdentity.init({container: '#identity-service'});
-  }, [null])
 
   return (
     <div className={styles.container}>
-      <Img fluid={props.file.childImageSharp.fluid} />
+      <div>logo</div>
       <div>Link</div>
       <div>Link</div>
       <div>Link</div>
-      <button id="identity-service" onClick={handleAdminClick}>Login with Netlify Identity</button>
       <div className={styles.emailContainer}>
         <input placeholder="Email" />
         <button >Signup</button>
@@ -34,18 +18,4 @@ const Nav = props => {
   );
 }
 
-export default () => (
-  <StaticQuery query={graphql`
-      query {
-        file(relativePath: { eq: "detail_-3.jpg" }) {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-      }
-    `}
-    render={Nav}
-  />
-);
+export default Nav;
