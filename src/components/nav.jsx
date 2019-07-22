8ffb5e972e3473d568c91bfc029b5b315bfb5227
Unknown <myles@mylesgearon.com>
2019-07-21 21:33:38 -0400

    Use netlifyIdentityWidget to guard /admin

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import netlifyIdentity from 'netlify-identity-widget';
 
 import styles from './nav.module.scss';
 
-const Nav = props => (
-  <div className={styles.container}>
-    <div>logo</div>
-    <div>Link</div>
-    <div>Link</div>
-    <div>Link</div>
-    <div className={styles.emailContainer}>
-      <input placeholder="Email" />
-      <button >Signup</button>
+const handleAdminClick = () => {
+  console.log(netlifyIdentity.currentUser());
+  if (netlifyIdentity.currentUser()) {
+    window.location.href = `${window.location.origin}/admin`;
+  } else {
+    netlifyIdentity.open();
+  }
+}
+
+const Nav = props => {
+  useEffect(() => {
+    netlifyIdentity.init({container: '#identity-service'});
+  }, [null])
+
+  return (
+    <div className={styles.container}>
+      <div>logo</div>
+      <div>Link</div>
+      <div>Link</div>
+      <div>Link</div>
+      <button id="identity-service" onClick={handleAdminClick}>Login with Netlify Identity</button>
+      <div className={styles.emailContainer}>
+        <input placeholder="Email" />
+        <button >Signup</button>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Nav;
