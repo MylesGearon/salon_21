71032a6e91050d7f1e168c6caf0084f914e5895e
Unknown <myles@mylesgearon.com>
2019-08-22 14:05:30 -0400

    1st pass at mobile concert cards

--- src/pages/concerts/ConcertCard.jsx
+++ src/pages/concerts/ConcertCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import p from 'prop-types';
 import fitty from 'fitty';
 import Img from 'gatsby-image';
@@ -27,24 +27,39 @@ const ConcertCard = ({concert}) => {
 
   return (
     <div className={styles.container}>
-      <Img
-        fluid={landingPageImage.childImageSharp.fluid}
-        fadeIn={true}
-        className={styles.img}
-      />
-      <div className={styles.concertInfoRow}>
+      <div>
+        <Img
+          fluid={landingPageImage.childImageSharp.fluid}
+          fadeIn={true}
+          className={styles.img}
+        />
+        <div className={styles.titlePositioner}>
+          <div className={styles.titleContainer}>
+            <h2 className={styles.title}>{title}</h2>
+          </div>
+        </div>
+      </div>
+      <div className={styles.concertInfo}>
         <div className={styles.dateContainer}>
           <h3 className={styles.dateNumber}>{concert.parsedDate.format('D')}</h3>
           <br className={styles.dateBreak} />
-          <h4 className={styles.dateMonth}>{concert.parsedDate.format('MMMM')}</h4>
+          <h4 className={styles.dateMonth}>{concert.parsedDate.format('MMM')}</h4>
         </div>
-        <div className={styles.titleContainer}>
-          <h2 className={styles.title}>{title}</h2>
+        <div className={styles.details}>
+          <p>
+            {concert.parsedDate.format('h:sA')} at <a className={styles.location} target="_blank" href="https://maps.google.com">The Westin Gallery</a>
+          </p>
+          {artists.map(artist => (
+            <Fragment>
+              <br />
+              {artist}
+            </Fragment>
+          ))}
         </div>
       </div>
       <div className={styles.buttonContainer}>
         <button className={[styles.button, styles.detailsButton].join(' ')} >Details</button>
-        <button className={[styles.button, styles.butButton].join(' ')} >Buy Now</button>
+        <button className={[styles.button, styles.buyButton].join(' ')} >Buy Now</button>
       </div>
     </div>
   );
