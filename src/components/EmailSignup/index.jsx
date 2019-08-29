4e8be2f5a54fb486d1570c7abab7221959743647
Unknown <myles@mylesgearon.com>
2019-08-29 03:33:11 -0400

    Wrap the email signup in the netlify form

--- src/components/EmailSignup/index.jsx
+++ src/components/EmailSignup/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import styles from './index.module.scss';
 
 export default () => (
-  <div className={styles.container}>
-    <input className={styles.input} placeholder="Email" />
-    <button className={styles.submit} type="submit">Keep In Touch!</button>
-  </div>
+    <form name="newsletter_signup" method="POST" data-netlify="true" className={styles.container}>
+      <input className={styles.input} placeholder="Email" name="email" />
+      <button className={styles.submit} type="submit">Keep In Touch!</button>
+    </form>
 );
