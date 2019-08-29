41cbe0a45721da8dea1a6d03788669b2b5cdd2ce
Unknown <myles@mylesgearon.com>
2019-08-29 03:48:52 -0400

    Try to stop netlify from removing my form input

--- src/components/EmailSignup/index.jsx
+++ src/components/EmailSignup/index.jsx
@@ -4,7 +4,11 @@ import styles from './index.module.scss';
 
 export default () => (
     <form name="newsletter_signup" method="POST" data-netlify="true" className={styles.container}>
-      <input className={styles.input} placeholder="Email" name="email" />
-      <button className={styles.submit} type="submit">Keep In Touch!</button>
+      <p>
+        <label><span style={{display: 'none'}}>Email: </span><input className={styles.input} placeholder="Email" name="email" /></label>
+      </p>
+      <p>
+        <button className={styles.submit} type="submit">Keep In Touch!</button>
+      </p>
     </form>
 );
