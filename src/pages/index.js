607bee950683bfb06c45a7fa8d7b3d901ebbe490
Unknown <myles@mylesgearon.com>
2019-10-07 16:19:37 -0400

    Styling tweaks for landing page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 import moment from "moment"
 import { Helmet } from "react-helmet"
-import { FiChevronsRight } from 'react-icons/fi';
+import { FiChevronRight as RightChevron } from 'react-icons/fi';
 
 import AppContainer from "../components/AppContainer"
 import styles from "./index.module.scss"
@@ -25,7 +25,6 @@ export default ({ data, path }) => {
       ticketLink
     }
   } = nextConcert;
-  console.log(title);
 
   return (
     <Fragment>
@@ -44,14 +43,21 @@ export default ({ data, path }) => {
           ]}
           fadeIn={true}
         />
-        <div className={styles.titleContainer}>
-          <Link to={`concerts/${nextConcert.frontmatter.path}`}>
+        <Link to={`concerts/${nextConcert.frontmatter.path}`}>
+          <div className={styles.titleContainer}>
             <div className={styles.info}>
               <h1 className={styles.title}>{title}</h1>
             </div>
-          </Link>
-          <h2 className={styles.buyLink}>Details</h2>
-        </div>
+            <div className={styles.dateInfoContainer}>
+              <h3 className={styles.date}>
+                {moment(datetime).format('MMM')}
+                <br/>
+                {moment(datetime).format('DD')}
+              </h3>
+              <RightChevron className={styles.concertLinkIcon} />
+            </div>
+          </div>
+        </Link>
       </AppContainer>
     </Fragment>
   );
